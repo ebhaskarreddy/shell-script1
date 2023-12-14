0d98cdc328ace38f15cbef901dfb26ac16ea1600
ebhaskarreddy <ebhaskarreddy967@gmail.com>
2023-12-14 14:36:17 +0530

    args

--- arguments.sh
+++ arguments.sh
@@ -10,3 +10,11 @@ echo "$PERSON1: Hello $PERSON2, Good Morning"
 echo "$PERSON2: Hi $PERSON1, very Good Morning"
 echo "$PERSON1: How are you $PERSON2?"
 echo "$PERSON2: I am good $PERSON1, How are you"
+
+echo "please enter the username"
+
+read -s USERNAME
+
+echo "please enter psswd"
+
+read -s PASSWD
